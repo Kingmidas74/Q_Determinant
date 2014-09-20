de3c1546710573556b15768316e537f0f51e2d3f
Midas <kingmidas1992@gmail.com>
2014-09-21 02:05:57 +0600

    v 0.0.0.1

--- FlowChart/IFlowChart.cs
+++ FlowChart/IFlowChart.cs
@@ -1,7 +1,23 @@
-﻿namespace FlowChart
+using FlowChart.AllConverters;
+
+namespace FlowChart
 {
     public interface IFlowChart
     {
-        
+        void ChartFromFile(ConverterTypes format, string filepath);
+        void ChartFromString(ConverterTypes format, string originalString);
+        int GetCountBlocks();
+        int GetCountLinks();
+        void SaveToFile(ConverterTypes format, string filePath);
+        string GetAsText(ConverterTypes format);
+        string GetMatrixToString();
+        void RemoveBlock(ulong id);
+        void RemoveLink(ulong from, ulong to);
+        void AddBlock(BlockTypes type, string content);
+        void AddLink(ulong from, ulong to, BrunchTypes type);
+        void ChangeContentBlock(ulong id, string content);
+        void ChangeTypeBlock(ulong id, BlockTypes type);
+        void ChangePropertiesLink(ulong oldFrom, ulong oldTo, ulong newFrom, ulong newTo);
+        bool CheckIntegrityScheme();
     }
 }

134b9c6a8f469b709bf4c95933582f705ce962cc
Midas <kingmidas1992@gmail.com>
2014-09-27 17:57:37 +0600

    small refactoring

--- Core/QTerm.cs
+++ Core/QTerm.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core
+﻿namespace Core
 {
     public class QTerm
     {
-        public string logical;
-        public string definitive;
+        public string Logical;
+        public string Definitive;
     }
 }

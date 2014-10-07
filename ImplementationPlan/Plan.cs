5f97f82ea7aebf46b684b0149df48082fb6f3079
Midas <kingmidas1992@gmail.com>
2014-10-08 01:25:34 +0600

    add Optimize for MaxEffective

--- ImplementationPlan/Plan.cs
+++ ImplementationPlan/Plan.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Core;
 
 namespace ImplementationPlan
 {
     public class Plan
     {
-        private readonly Graph _graph;
+        private Graph _graph;
 
         public ulong CountProcessors { get; private set; }
         public ulong CountTacts { get; private set; }
@@ -15,6 +16,53 @@ namespace ImplementationPlan
             var transformer = new Transformer(qDet, operations);
             _graph = transformer.Graph;
             CountProcessors = _graph.GetMaxOperationsInLevel();
+            CountTacts = _graph.GetMaxLevel();
+        }
+
+        public Plan(List<Operation> operations, QDet qDet, ulong countProcessors)
+        {
+            var transformer = new Transformer(qDet, operations);
+            _graph = transformer.Graph;
+            OptimizeForMaxEffective(countProcessors);
+        }
+
+        public void OptimizeForMaxEffective(ulong countProcessors)
+        {
+            var blocks = (GetVertexGraph()).Where(x => x.Level > 0).OrderBy(x=>x.Level).ThenBy(x=>x.Id);
+            var links = GetEdgesGraph();
+            var nBlocks = new List<Block>();
+            ulong currentNewLevel = 1;
+            var tempBlock = blocks.First();
+            nBlocks.Add(tempBlock);
+            blocks = blocks.Where(x => x.Id != tempBlock.Id).OrderBy(x => x.Level).ThenBy(x => x.Id);
+            foreach (var block in blocks)
+            {
+                var isNewLevel = (ulong)nBlocks.LongCount(x => x.Level == currentNewLevel) < countProcessors == false;
+                foreach (var link in links)
+                {
+                    if (link.To == block.Id)
+                    {
+                        if (blocks.FirstOrDefault(x => x.Id == link.From) != null)
+                        {
+                            tempBlock = blocks.FirstOrDefault(y => y.Id == link.From);
+                            if (tempBlock != null && tempBlock.Level == currentNewLevel)
+                            {
+                                isNewLevel = true;
+                                break;
+                            }
+                        }
+                        
+                    }
+                }
+                if (isNewLevel == true)
+                {
+                    currentNewLevel++;
+                }
+                block.Level = currentNewLevel;
+                nBlocks.Add(block);
+            }
+            _graph = new Graph(nBlocks,links);
+            CountProcessors = countProcessors;
             CountTacts = _graph.GetMaxLevel();
         }
 
--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -40,16 +40,27 @@ namespace UnitTest
                 QDeterminant = new List<QTerm>()
             };
             qDet.QDeterminant.Add(new QTerm() { Definitive = "(8+2*5)/(1+3*2-4)", Logical = "dX>=(5*a+2*(b-1))" });
-            qDet.QDeterminant.Add(new QTerm() { Definitive = "x1*y1+x2*y2+x3*y3", Logical = "" });
+          //  qDet.QDeterminant.Add(new QTerm() { Definitive = "x1*y1+x2*y2+x3*y3", Logical = "" });
             var converter = Manufactory.CreateOperationConverter(ConverterTypes.JSON);
             converter.ParseDocument(Jsonpathfolder+@"test3.json");
             var plan = new Plan(converter.GetBlocks(), qDet);
+            Assert.AreEqual((ulong)4, plan.CountProcessors);
+            Assert.AreEqual((ulong)4, plan.CountTacts);
+            //plan.OptimizeForMaxEffective(3);
+            plan = new Plan(converter.GetBlocks(), qDet,3);
+            Assert.AreEqual((ulong)3, plan.CountProcessors);
+            Assert.AreEqual((ulong)5, plan.CountTacts);
+          /*  plan.OptimizeForMaxEffective(2);
+            Assert.AreEqual((ulong)2, plan.CountProcessors);
+            Assert.AreEqual((ulong)6, plan.CountTacts);
+            plan.OptimizeForMaxEffective(1);
+            Assert.AreEqual((ulong)1, plan.CountProcessors);
+            Assert.AreEqual((ulong)11, plan.CountTacts);*/
             var saver = Manufactory.CreateImplementationPlanConverter(ConverterTypes.JSON);
             saver.SetBlocks(plan.GetVertexGraph());
             saver.SetLinks(plan.GetEdgesGraph());
             saver.SaveToFile(Jsonpathfolder + @"output_plan.json");
-            Assert.AreEqual((ulong)7,plan.CountProcessors);
-            Assert.AreEqual((ulong)4,plan.CountTacts);
+            
         }
     }
 }

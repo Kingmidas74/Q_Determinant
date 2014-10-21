ea68a7cf7ee22c7fe3cab062eeafe11c7d3ea359
Ilya Sharabura <run174@yandex.ru>
2014-10-21 14:28:11 +0600

    AL3

---
+++ ActionList/AList.cs
@@ -0,0 +1,33 @@
+﻿using Core;
+using System.Collections.Generic;
+namespace ActionList
+{
+    public class AList: IAList
+    {
+        public QDet AL;
+        public AList(List<Block> Blocks, List<Link> Links, List<Operation> Oper)
+        {
+            var x = new QTerm();
+            var y = new QTerm();
+            var z = new QTerm();
+            x.Logical = "dx>=(5*a+2*(b-1))";
+            x.Definitive = "(8+2*5)/(1+3*2-4)";
+            AL.QDeterminant.Add(x);
+            y.Definitive = "3*x+1";
+            AL.QDeterminant.Add(y);
+            foreach (var example in Blocks)
+            {
+                if (example.Type == BlockTypes.Condition)
+                { 
+
+                }
+            }
+        }
+
+        public QDet getqdet()
+        {
+            return AL;
+        }
+    }
+
+}

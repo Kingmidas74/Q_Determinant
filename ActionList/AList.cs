070bac487185f28956d667770a85aaf4ddf02098
Ilya Sharabura <run174@yandex.ru>
2014-10-27 13:38:41 +0500

    ALv4

--- ActionList/AList.cs
+++ ActionList/AList.cs
@@ -1,5 +1,7 @@
 ﻿using Core;
 using System.Collections.Generic;
+using System.Linq;
+
 namespace ActionList
 {
     public class AList: IAList
@@ -7,7 +9,10 @@ namespace ActionList
         public QDet AL;
         public AList(List<Block> Blocks, List<Link> Links, List<Operation> Oper)
         {
-            var x = new QTerm();
+
+            Link FLink;
+            FLink = null; 
+           /* var x = new QTerm();
             var y = new QTerm();
             var z = new QTerm();
             x.Logical = "dx>=(5*a+2*(b-1))";
@@ -21,13 +26,93 @@ namespace ActionList
                 { 
 
                 }
+            }*/
+
+        foreach (var ex in Links)
+            {
+                var x = new QTerm();
+                var z = new QTerm();
+                if (ex.From == 1)
+                {
+                    var y = Blocks.FirstOrDefault(e => e.Id == ex.To);
+                    if (y != null)
+                    { 
+                        if (y.Type == BlockTypes.Process)
+                        {
+                            x.Definitive = y.Content;
+                        }
+                        if (y.Type == BlockTypes.Condition)
+                        {
+                            z = x;
+                            x.Logical = y.Content;
+                           // z.Logical = 
+                        }
+                    }
+                    
+                }
+                FLink = ex;
             }
+        if (FLink != null)
+        {
+            QQ(Blocks, Links, FLink);
+        }
+        }
+
+        private void QQ(List<Block> Blocks, List<Link> Links, Link l)
+        {
+            var x = new QTerm();
+            var z = new QTerm();
+            Link t;
+            Link t1;
+            t1 = null;
+            t = null;
+            var y = Blocks.FirstOrDefault(e => e.Id == l.To);
+            if (y != null)
+            {
+                if (y.Type == BlockTypes.Process)
+                {
+                    x.Definitive = y.Content;
+                    AL.QDeterminant.Add(x);
+                    t = Links.FirstOrDefault(e => e.From == y.Id);
+                    QQ(Blocks, Links, t);
+                }
+                if (y.Type == BlockTypes.Condition)
+                {
+                    foreach (var m in Links)
+                    {
+                        if (m.From == y.Id)
+                            t = m;
+                    }
+                    foreach (var m in Links)
+                        if ((m.From == y.Id) && (m != t))
+                            t1 = m;
+                    z = x;
+                    x.Logical = y.Content;
+                    AL.QDeterminant.Add(x);
+                    // z.Logical = 
+                    //AL.QDeterminant.Add(z);
+                    QQ(Blocks, Links, t);
+                    QQ(Blocks, Links, t1);
+                }
+                if ((y.Type == BlockTypes.Input) && (y.Type == BlockTypes.Output))
+                {
+                    t = Links.FirstOrDefault(e => e.From == y.Id);
+                    QQ(Blocks, Links, t);
+                }
+            }
+   
+        }
+
+        private void pars()
+        { 
         }
 
         public QDet getqdet()
         {
             return AL;
         }
+
+       // public bool ex { get; set; }
     }
 
 }

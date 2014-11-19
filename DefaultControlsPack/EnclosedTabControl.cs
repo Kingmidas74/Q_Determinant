267e6257d53d8f38d4fef45e85727972047d7729
Midas <kingmidas1992@gmail.com>
2014-11-19 22:35:01 +0500

    Сделал вкладки закрывающимися. И добавил парочки контролов

---
+++ DefaultControlsPack/EnclosedTabControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DefaultControlsPack.Enums;
+
+namespace DefaultControlsPack
+{
+    public class EnclosedTabControl : TabControl
+    {
+
+        #region VerticalContentPosition
+        public static readonly DependencyProperty VerticalContentPositionProperty =
+            DependencyProperty.Register("VerticalContentPosition", typeof(VerticalContentPosition), typeof(TabControl),
+                new FrameworkPropertyMetadata());
+
+        public VerticalContentPosition VerticalContentPosition
+        {
+            get { return (VerticalContentPosition)GetValue(VerticalContentPositionProperty); }
+            set { SetValue(VerticalContentPositionProperty, value); }
+        }
+        #endregion
+
+        public EnclosedTabControl()
+        {
+            AddHandler(EnclosedTabItem.CloseTabEvent, new RoutedEventHandler(CloseTab));
+        }
+
+        private void CloseTab(object source, RoutedEventArgs args)
+        {
+            var item = args.OriginalSource as EnclosedTabItem;
+            Items.Remove(item);
+        }
+    }
+}
---
+++ DefaultControlsPack/EnclosedTabItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DefaultControlsPack
+{
+    public class EnclosedTabItem : TabItem
+    {
+        #region CloseTabEvent
+        public static readonly RoutedEvent CloseTabEvent = EventManager.RegisterRoutedEvent("CloseTab",
+            RoutingStrategy.Bubble, typeof (RoutedEventHandler), typeof (EnclosedTabItem));
+
+        public event RoutedEventHandler CloseTab
+        {
+            add { AddHandler(CloseTabEvent, value); }
+            remove { RemoveHandler(CloseTabEvent, value); }
+        }
+        #endregion
+        
+        static EnclosedTabItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EnclosedTabItem), new FrameworkPropertyMetadata(typeof(EnclosedTabItem)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            (GetTemplateChild("CloseTabButton") as Button).Click += CloseTabButtonClick;
+            base.OnApplyTemplate();
+        }
+
+        void CloseTabButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.RaiseEvent(new RoutedEventArgs(CloseTabEvent, this));
+        }
+    }
+}

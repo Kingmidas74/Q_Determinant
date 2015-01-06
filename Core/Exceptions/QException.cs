13b8c1747a1033ea1a1daa856fda4cd84d510c42
Midas <kingmidas1992@gmail.com>
2015-01-06 19:28:21 +0500

    Добавил класс исключений

---
+++ Core/Exceptions/QException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Core.Exceptions
+{
+    public class QException:Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return base.Message;
+            }
+        }
+        public QException()
+        {
+
+        }
+
+        public QException(string message)
+        {
+            
+
+        }
+    }
+}

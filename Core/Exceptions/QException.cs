7366e9705ddbb456bf610918c5ea4127673f2ba7
Midas <kingmidas1992@gmail.com>
2015-01-07 17:55:12 +0500

    Сделал кастомное исключение сериализуемым

--- Core/Exceptions/QException.cs
+++ Core/Exceptions/QException.cs
@@ -2,24 +2,29 @@
 
 namespace Core.Exceptions
 {
+    [Serializable]
     public class QException:Exception
     {
-        public override string Message
-        {
-            get
-            {
-                return base.Message;
-            }
-        }
         public QException()
         {
-
+            
         }
 
-        public QException(string message)
+        public QException(string message) : base(message)
         {
             
+        }
 
+        public QException(string message, Exception ex) : base(message)
+        {
+            
+        }
+
+        protected QException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext contex)
+            : base(info, contex)
+        {
+            
         }
     }
 }

d0dd1de32fcd061bc2ac7f13d5bb50e71df87922
Midas <kingmidas1992@gmail.com>
2015-01-08 20:52:51 +0500

    Теперь коммиты будут ещё чаще :(

--- QDeterminant/Determinant.cs
+++ QDeterminant/Determinant.cs
@@ -52,6 +52,7 @@ namespace QDeterminant
 
         public void CalculateDeterminant()
         {
+
             _qDeterminantStandart.Add(new QTerm {Definitive = "(5+3+8)/(3*5-7)+3*5", Logical = "5+5+5+5>=8*(5+5+7+1)"});
             TransformDeterminant();
         }

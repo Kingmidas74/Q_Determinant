01e47a9bf4c592071778377cba745a6e2c9bd4f9
Midas <kingmidas1992@gmail.com>
2015-01-14 15:23:10 +0500

    Подправлен баг с прокруткой графа

---
+++ ImplementationPlanViewer/InternalClasses/LocalConverters/LevelToHeihtConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DefaultControlsPack.Converters;
+
+namespace ImplementationPlanViewer.InternalClasses.LocalConverters
+{
+    public class LevelToHeihtConverter : BaseConverter<LevelToHeihtConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((ulong) value)*30 + 4*30 + 40*((ulong) value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}

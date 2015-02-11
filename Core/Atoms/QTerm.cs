02cb1ebdd6c0188a7579f68e385897aaa7294356
Ilya Sharabura <run174@yandex.ru>
2015-02-11 11:18:57 +0500

    В Q-терм добавлены новые поля

--- Core/Atoms/QTerm.cs
+++ Core/Atoms/QTerm.cs
@@ -1,9 +1,16 @@
-﻿namespace Core.Atoms
+﻿using System.Collections.Generic;
+
+namespace Core.Atoms
 {
     public class QTerm
     {
         public string Logical { get; set; }
         public string Definitive { get; set; }
         public ulong Index { get; set; }
+
+        public List<string> LogicalChain { get; set; }
+        public List<string> DefinitiveChain { get; set; }
+
+
     }
 }
